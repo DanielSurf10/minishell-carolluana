784a3d2ef81136f13fae78d26402971f38a09919
Luana Souza Ribeiro <l182300@dac.unicamp.br>
2024-10-07 17:31:22 -0300

    sei lá

--- srcs/list.c
+++ srcs/list.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   list.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lsouza-r <lsouza-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:28:09 by cshingai          #+#    #+#             */
-/*   Updated: 2024/09/26 17:03:32 by cshingai         ###   ########.fr       */
+/*   Updated: 2024/10/07 17:30:10 by lsouza-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,91 +14,91 @@
 // mais modificações
 
 
-#include "../include/minishell.h"
+// #include "../include/minishell.h"
 
-/*
-	My intention here is to pass the copy_prompt, with spaces between the
-	metachars, to a double linked list. So I can iterate between the chars
-	to identify the tolkens and latter the necessary adaptations.
-*/
-//utils
-void	push_prompt_to_list(char **prompt)
-{
-	char	*cpy_prompt;
-	t_list	*list;
-	int		idx;
+// /*
+// 	My intention here is to pass the copy_prompt, with spaces between the
+// 	metachars, to a double linked list. So I can iterate between the chars
+// 	to identify the tolkens and latter the necessary adaptations.
+// */
+// //utils
+// void	push_prompt_to_list(char **prompt)
+// {
+// 	char	*cpy_prompt;
+// 	t_list	*list;
+// 	int		idx;
 
-	list = 0;
-	idx = 0;
-	cpy_prompt = malloc(sizeof (char) * (3 * strlen(*prompt) + 1));
-	strcpy_space(*prompt, cpy_prompt);
-	printf("prompt: %s\n", cpy_prompt);
-	while (cpy_prompt[idx])
-		create_list(&list, cpy_prompt[idx++]);
-	print_list(&list);
-	free(cpy_prompt);
-}
-//utils
-void	create_list(t_list **list, char c)
-{
-	t_list	*node;
-	t_list	*aux;
+// 	list = 0;
+// 	idx = 0;
+// 	cpy_prompt = malloc(sizeof (char) * (3 * strlen(*prompt) + 1));
+// 	strcpy_space(*prompt, cpy_prompt);
+// 	printf("prompt: %s\n", cpy_prompt);
+// 	while (cpy_prompt[idx])
+// 		create_list(&list, cpy_prompt[idx++]);
+// 	print_list(&list);
+// 	free(cpy_prompt);
+// }
+// //utils
+// void	create_list(t_list **list, char c)
+// {
+// 	t_list	*node;
+// 	t_list	*aux;
 
-	node = malloc(sizeof (t_list));
-	if (!node)
-		return ;
-	node->value = c;
-	node->next = NULL;
-	if (*list == NULL)
-	{
-		*list = node;
-		node->prev = NULL;
-	}
-	else
-	{
-		aux = *list;
-		while(aux->next)
-			aux = aux->next;
-		aux->next = node;
-		node->prev = aux;
-	}
-}
+// 	node = malloc(sizeof (t_list));
+// 	if (!node)
+// 		return ;
+// 	node->value = c;
+// 	node->next = NULL;
+// 	if (*list == NULL)
+// 	{
+// 		*list = node;
+// 		node->prev = NULL;
+// 	}
+// 	else
+// 	{
+// 		aux = *list;
+// 		while(aux->next)
+// 			aux = aux->next;
+// 		aux->next = node;
+// 		node->prev = aux;
+// 	}
+// }
 
-// Just a checker is the list is working
-// remover
-void	print_list(t_list **list)
-{
-	t_list *aux;
+// // Just a checker is the list is working
+// // remover
+// void	print_list(t_list **list)
+// {
+// 	t_list *aux;
 
-	aux = *list;
-	while (aux)
-	{
-		printf("list[%c]\n", aux->value);
-		aux = aux->next;
-	}
-}
+// 	aux = *list;
+// 	while (aux)
+// 	{
+// 		printf("list[%c]\n", aux->value);
+// 		aux = aux->next;
+// 	}
+// }
 
-// copy prompt with spaces between metachars
-// utils
-void	*strcpy_space(char *prompt, char *dest)
-{
-	int	p;
-	int	d;
+// // copy prompt with spaces between metachars
+// // utils
+// void	*strcpy_space(char *prompt, char *dest)
+// {
+// 	int	p;
+// 	int	d;
 
-	p = 0;
-	d = 0;
-	while(prompt[p])
-	{
-		if (is_metachar(prompt[p]))
-		{
-			if ( d > 0 && dest[d -1] != ' ')
-				dest[d++] = ' ';
-			dest[d++] = prompt[p++];
-			dest[d++] = ' ';
-		}
-		else
-			dest[d++] = prompt[p++];
-	}
-	dest[d] = '\0';
-	return (NULL);
-}
+// 	p = 0;
+// 	d = 0;
+// 	while(prompt[p])
+// 	{
+// 		if (is_metachar(prompt[p]))
+// 		{
+// 			if ( d > 0 && dest[d -1] != ' ')
+// 				dest[d++] = ' ';
+// 			dest[d++] = prompt[p++];
+// 			dest[d++] = ' ';
+// 		}
+// 		else
+// 			dest[d++] = prompt[p++];
+// 	}
+// 	dest[d] = '\0';
+// 	return (NULL);
+// }
--- srcs/metachar.c
+++ srcs/metachar.c
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   metachar.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lsouza-r <lsouza-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:06:08 by cshingai          #+#    #+#             */
-/*   Updated: 2024/09/23 18:24:19 by cshingai         ###   ########.fr       */
+/*   Updated: 2024/10/07 17:30:21 by lsouza-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_metachar(char c)
-{
-	if (c == '"')
-		return (2);
-	else if (c == '\'')
-		return (2);
-	else if (c == '|')
-		return (1);
-	else if (c == '<')
-		return (1);
-	else if (c == '>')
-		return (1);
-	else
-		return (0);
-}
+// int	is_metachar(char c)
+// {
+// 	if (c == '"')
+// 		return (2);
+// 	else if (c == '\'')
+// 		return (2);
+// 	else if (c == '|')
+// 		return (1);
+// 	else if (c == '<')
+// 		return (1);
+// 	else if (c == '>')
+// 		return (1);
+// 	else
+// 		return (0);
+// }
 
 // int	sep_signals(char c)
 // {
