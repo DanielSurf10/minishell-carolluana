2be81cd3aa56b142b12b74eac52af746f06404f0
DanielSurf10 <daniel101surf@gmail.com>
2024-12-31 15:22:58 -0300

    adicionando obersavação no echo

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -24,6 +24,10 @@ int	echo(char **arg)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 		return (0);
 	}
+
+	// Aqui tem que ver se vai aceitar "-nnnn" e "-ne"
+	// Ou só "-n" e "-n -n"
+
 	while (arg[i] && ft_strncmp(arg[i], "-n", 2) == 0)
 	{
 		has_flag = 1;
