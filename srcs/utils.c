3f826f5534b35924aa33f14f48b57c25fdc982b7
CarolShingai <carolineshingai@gmail.com>
2025-01-05 18:34:25 -0300

    path inicializado no init_shell

--- srcs/utils.c
+++ srcs/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsouza-r <lsouza-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cshingai <cshingai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:06:08 by cshingai          #+#    #+#             */
-/*   Updated: 2024/12/28 17:23:08 by lsouza-r         ###   ########.fr       */
+/*   Updated: 2025/01/05 18:25:14 by cshingai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ void	init_shell(t_minishell *shell)
 	shell->status = 0;
 	shell->pid = NULL;
 	shell->fd_list = NULL;
+	shell->path = NULL;
 	g_signal = 0;
 	shell->fd_stdin = dup(STDIN_FILENO);
 	shell->fd_stdout = dup(STDOUT_FILENO);
